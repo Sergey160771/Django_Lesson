1cfa0ec956722c0a7ab78630e0b1bcb3333a9bc3
Sergey160771 <9058591133@mail.ru>
2026-04-23 11:39:54 +0500

    Первый комит

---
+++ myapp2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Myap2Config(AppConfig):
+    name = 'myapp2'
---
+++ myapp4/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import user_form, many_fields_form, many_fields_form_widget, add_user, upload_image
+
+urlpatterns = [
+    path('user/add/', user_form, name='user_form'),
+    path('forms/', many_fields_form, name='many_fields_form'),
+    path('formsw/', many_fields_form_widget, name='many_fields_form_widget'),
+    path('user/', add_user, name='add_user'),
+    path('upload/', upload_image, name='upload_image'),
+]
